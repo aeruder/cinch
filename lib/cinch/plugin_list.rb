4227b5ef3bcfd125b12beb41efe33b503e7bbd81
Andrew Ruder <andy@aeruder.net>
2016-08-10 14:19:27 -0500

    Try to add in multi-thread access to plugin list

--- lib/cinch/plugin_list.rb
+++ lib/cinch/plugin_list.rb
@@ -1,33 +1,52 @@
+require 'monitor'
+
 module Cinch
   # @since 2.0.0
   class PluginList < Array
+    attr_accessor :monitor
+
     def initialize(bot)
       @bot     = bot
+      @monitor   = Monitor.new
       super()
     end
 
     # @param [Class<Plugin>] plugin
     def register_plugin(plugin)
-      self << plugin.new(@bot)
+      monitor.synchronize {
+        self << plugin.new(@bot)
+      }
     end
 
     # @param [Array<Class<Plugin>>] plugins
     def register_plugins(plugins)
-      plugins.each { |plugin| register_plugin(plugin) }
+      monitor.synchronize {
+        plugins.each { |plugin| register_plugin(plugin) }
+      }
     end
 
     # @since 2.0.0
     def unregister_plugin(plugin)
-      plugin.unregister
-      delete(plugin)
+      monitor.synchronize {
+        plugin.unregister
+        delete(plugin)
+      }
+    end
+
+    def unregister_plugin_class(plugin)
+      monitor.synchronize {
+        self.dup.each { |x| unregister_plugin(x) if x.is_a? plugin }
+      }
     end
 
     # @since 2.0.0
     def unregister_plugins(plugins)
-      if plugins == self
-        plugins = self.dup
-      end
-      plugins.each { |plugin| unregister_plugin(plugin) }
+      monitor.synchronize {
+        if plugins == self
+          plugins = self.dup
+        end
+        plugins.each { |plugin| unregister_plugin(plugin) }
+      }
     end
 
     # @since 2.0.0
